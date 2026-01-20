779e8819637f7176d834e02ec4283f4cd822f7a7
Praveen Reddy <2400032662@kluniversity.in>
2026-01-20 08:24:25 +0530

    20-01-2026 Diff b/n ApplicationContextXML, AnnotationConfig
    Ecommerce App of sping using two diff config - 
     One with Application Context
     One with Annotation Config

---
+++ DI-ECommerce1/src/main/java/com/klu/main/MainApp.java
@@ -0,0 +1,25 @@
+package com.klu.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.klu.config.AppConfig;
+import com.klu.model.ProductOrder;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context =	new ClassPathXmlApplicationContext("applicationContext.xml");
+		ProductOrder p= (ProductOrder)context.getBean("order");
+		p.display();
+		
+		ApplicationContext context1 = new AnnotationConfigApplicationContext(AppConfig.class);
+		ProductOrder p1 = context1.getBean(ProductOrder.class);
+		
+		p1.display();
+		
+	}
+
+}
