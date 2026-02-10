e5e58055f87d9afcc0f3cff541b1cd009b490df5
Praveen Reddy <2400032662@kluniversity.in>
2026-02-10 11:34:13 +0530

    10-02-2026 In-Class Controller,Model demo

---
+++ 2SpringBootDemo/src/test/java/com/klu/Spring_Dependency_Injection/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klu.Spring_Dependency_Injection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
